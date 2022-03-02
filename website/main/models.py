3deef0922185155829862c5daf7abfbd64a9a846
uatragada <42359216+uatragada@users.noreply.github.com>
2022-03-02 17:57:47 -0500

    main

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name  = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length= 9)
