f7aad68be6d0aace79517cc00308318c1877d1f4
uatragada <42359216+uatragada@users.noreply.github.com>
2022-03-02 18:15:00 -0500

    main

--- website/main/models.py
+++ website/main/models.py
@@ -6,5 +6,19 @@ class User(models.Model):
     password = models.CharField(max_length=30)
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length= 9)
-    user_type = models.Model()
-    
+    provider_permissions = models.OneToOneField(ServiceProvider)
+    client_permissions = models.OneToOneField(Client)
+    thirdParty_permissions = models.OneToOneField(thirdParty)
+
+class ServiceProvider(models.Model):
+    provider_name = models.CharField(max_length= 30)
+    website = models.CharField(max_length= 50)
+
+class Client(models.Model):
+    client_name: models.CharField(max_length= 30)
+    date_of_birth: models.DateField()
+
+class thirdParty(models.Model) : 
+    third_party_name = models.CharField(max_length= 30)
+    website = models.CharField(max_length= 50)
+    category = models.Model()
