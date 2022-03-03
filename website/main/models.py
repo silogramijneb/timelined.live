884fadae95e650784864a8abd4b8319e13ab2b8b
Ubuntu <ubuntu@ubuntu.myfiosgateway.com>
2022-03-03 00:06:12 +0000

    update main models to correct syntax

--- website/main/models.py
+++ website/main/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    user_name  = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    address = models.CharField(max_length=100)
-    phone = models.CharField(max_length= 9)
-    provider_permissions = models.OneToOneField(ServiceProvider)
-    client_permissions = models.OneToOneField(Client)
-    thirdParty_permissions = models.OneToOneField(thirdParty)
-
 class ServiceProvider(models.Model):
     provider_name = models.CharField(max_length= 30)
     website = models.CharField(max_length= 50)
@@ -21,4 +12,14 @@ class Client(models.Model):
 class thirdParty(models.Model) : 
     third_party_name = models.CharField(max_length= 30)
     website = models.CharField(max_length= 50)
-    category = models.Model()
+    #category = models.Model()
+
+class User(models.Model):
+    user_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=9)
+    provider_permissions = models.OneToOneField(ServiceProvider, on_delete=models.CASCADE)
+    client_permissions = models.OneToOneField(Client, on_delete=models.CASCADE)
+    thirdParty_permissions = models.OneToOneField(thirdParty, on_delete=models.CASCADE)
+
