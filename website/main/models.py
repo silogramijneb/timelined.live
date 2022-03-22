f895edd07db6cafff94c87561fb50cd29663a79e
uatragada <42359216+uatragada@users.noreply.github.com>
2022-03-22 18:06:34 -0400

    main

--- website/main/models.py
+++ website/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
 class ServiceProvider(models.Model):
@@ -23,3 +24,24 @@ class User(models.Model):
     client_permissions = models.OneToOneField(Client, on_delete=models.CASCADE)
     thirdParty_permissions = models.OneToOneField(thirdParty, on_delete=models.CASCADE)
 
+class Timeline(models.Model):
+    events = models.oneToManyField(Event, on_delete = models.CASCADE)
+    users = models.oneToManyField(User, on_delete = models.CASCADE)
+
+
+class Event(models.Model):
+    status = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 256)
+    location = models.CharField(max_length = 100)
+    date_created = models.DateField()
+    deadline = models.DateField()
+    date_modified = models.DateField()
+    date_ended = models.DateField()
+    collaborators = models.oneToManyField(User, on_delete = models.CASCADE)
+    images = models.oneToManyField(Image, on_delete = models.CASCADE)
+
+class Image(model.Model):
+    image = models.ImageField(upload_to = 'uploads/')
+
+class Document(models.Model)
+    
