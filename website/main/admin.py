b2e2fc02ea601523dc885bb2aadfae8905ae4206
uatragada <42359216+uatragada@users.noreply.github.com>
2022-03-24 18:05:12 -0400

    Update admin.py

--- website/main/admin.py
+++ website/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import ServiceProvider, Client, thirdParty, User, Timeline, Event, Image, Document
 # Register your models here.
+admin.site.Register(ServiceProvider)
+admin.site.Register(Client)
+admin.site.Register(thirdParty)
+admin.site.Register(User)
+admin.site.Register(Timeline)
+admin.site.Register(Event)
+admin.site.Register(Image)
+admin.site.Register(Document)
