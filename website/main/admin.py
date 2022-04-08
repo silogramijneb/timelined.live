528f764d6d45d47e61cda2d6305c6d36f6369a7c
uatragada <42359216+uatragada@users.noreply.github.com>
2022-04-08 16:48:59 -0400

    main

--- website/main/admin.py
+++ website/main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 #from .models import ServiceProvider, Client, thirdParty, User, Timeline, Event, Image, Document
 
-'''
+
 # Register your models here.
 admin.site.Register(ServiceProvider)
 admin.site.Register(Client)
@@ -11,4 +11,4 @@ admin.site.Register(Timeline)
 admin.site.Register(Event)
 admin.site.Register(Image)
 admin.site.Register(Document)
-'''
+
