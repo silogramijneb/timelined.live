85ca342472c237b23a2b8c2a4298542aec0613d9
Ivan Bondarenko <ivanbonddev@gmail.com>
2022-04-18 18:46:19 -0400

    update

--- website/servicepro/views.py
+++ website/servicepro/views.py
@@ -2,5 +2,3 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def dashboard(response):
-    return render(response, 'templates/dashboard_main.html')
