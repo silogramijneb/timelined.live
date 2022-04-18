041ec4ce70126ac345771b0676ba7fc3b7449ce7
Ivan Bondarenko <ivanbonddev@gmail.com>
2022-04-18 15:58:07 -0400

    update

--- website/main/admin.py
+++ website/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ServiceProvider, Client, thirdParty, User, Timeline, Event, Image, Document
+from .models import *
 
 
 # Register your models here.
