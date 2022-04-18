3cf0ecd1e5084457bf64f2480adb4ccb306f2d9c
Ivan Bondarenko <ivanbonddev@gmail.com>
2022-04-18 16:00:25 -0400

    update

--- website/main/admin.py
+++ website/main/admin.py
@@ -5,10 +5,10 @@ from .models import *
 # Register your models here.
 admin.site.register(ServiceProvider)
 admin.site.register(Client)
-admin.site.register(thirdParty)
+#admin.site.register(thirdParty)
 admin.site.register(User)
 admin.site.register(Timeline)
 admin.site.register(Event)
-admin.site.register(Image)
+#admin.site.register(Image)
 admin.site.register(Document)
 
