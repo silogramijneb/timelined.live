3e4b7b14de334b13b01baa70f583a6a0ec5490ad
Ivan Bondarenko <ivanbonddev@gmail.com>
2022-04-18 16:10:55 -0400

    update

--- website/main/models.py
+++ website/main/models.py
@@ -1,6 +1,6 @@
-from datetime import date, timezone
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -34,7 +34,7 @@ class Client(Profile):
         db_table = 'Client'
 
     models.CharField(max_length= 30)
-    date_of_birth = models.DateField(default=date.today())
+    date_of_birth = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
