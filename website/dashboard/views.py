62e015b2fdd968e66563c665973c6bcfd4fc4b1d
Ivan Bondarenko <ivanbonddev@gmail.com>
2022-04-19 01:05:29 -0400

    update

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, request
 from django.http import HttpResponse
+
+from main.forms import EventCreationForm
+
+# Sends user data from timeline event to database
+def updateEvent(request, context):
+    form = EventCreationForm(request.POST)
+    if form.is_valid():
+        event = form.save(commit=False)
+        event.save()
+    context.update({"event_creation_form": form})
 
 # Create your views here.
 def dashboard(response):
     return render(response, "dashboard/home.html")
 
-def timeline(response):
-    return render(response, "dashboard/timeline.html")
+def timeline(request):
+    
+    context = {}
+    context.update({"event_creation_form": EventCreationForm()})
+    
+    # POST: Update context
+    if request.method == "POST":
+        if request.POST.get("action") == 'update':
+            updateEvent(request, context)
+            
+    result = render(request, "dashboard/timeline.html", context)            
+    return result
