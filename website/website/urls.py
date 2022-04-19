07f2963685e2e33b3d43d364d38724e6e1ef3984
Ivan Bondarenko <ivanbonddev@gmail.com>
2022-04-18 23:20:43 -0400

    update

--- website/website/urls.py
+++ website/website/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")), # index view (front page)
-    path('dashboard/', include("dashboard.urls")), # dashboard
 ]
