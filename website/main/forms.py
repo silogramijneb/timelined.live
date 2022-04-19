665328836cfaf7dffa9636e4525fa01ff2531180
Winston <winstonobonsu@gmail.com>
2022-04-19 09:05:51 -0400

    Installed Crispy and CrispyBootstrap5; Updated forms to preload CSS attributes

--- website/main/forms.py
+++ website/main/forms.py
@@ -1,24 +1,106 @@
 from dataclasses import field
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .models import *
-
+from .models import ThirdParty, ServiceProvider, Client, Timeline, Event
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Column, Row
+from crispy_bootstrap5.bootstrap5 import FloatingField
 
 class ClientRegistrationForm(UserCreationForm):
     class Meta:
         model = Client
-        fields = ['last_name', 'first_name', 'date_of_birth', 'username', 'password1', 'password2', 'phone']
+        fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'password1', 'password2']
+        labels = {
+            'first_name': 'First Name',
+            'last_name' : 'Last Name',
+            'email' : 'Email Address',
+        } # Password1 and Password2 must be relabled outside of lables
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Crispy Package Setup
+        self.helper = FormHelper()
+
+        # Bootstrap 
+        self.helper.layout = Layout(
+            FloatingField('first_name', css_class='mb-3'),
+            FloatingField('last_name', css_class='mb-3'),
+            FloatingField('date_of_birth', css_class='mb-3'),
+            FloatingField('email', css_class='mb-3'),
+            FloatingField('password1', css_class='mb-3'),
+            FloatingField('password2', css_class='mb-3')
+        )
+            
+        # Django form attributes / Field properties
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+        self.fields['password2'].label = 'Password Confirmation'
+        self.fields['date_of_birth'].widget = DateInput()
 
 class ProfessionalRegistrationForm(UserCreationForm):
     class Meta:
         model = ServiceProvider
         fields = ['first_name', 'last_name', 'provider_name', 'website', 'username', 'password1', 'password2']
+        labels = {
+            'first_name': 'First Name',
+            'last_name' : 'Last Name',
+            'email' : 'Email Address',
+        } # Password1 and Password2 must be relabled outside of lables
 
-class ThirdPartyRegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Crispy Package Setup
+        self.helper = FormHelper()
+
+        # Bootstrap 
+        self.helper.layout = Layout(
+            FloatingField('first_name', css_class='mb-3'),
+            FloatingField('last_name', css_class='mb-3'),
+            FloatingField('provider_name', css_class='mb-3'),
+            FloatingField('website', css_class='mb-3'),
+            FloatingField('email', css_class='mb-3'),
+            FloatingField('password1', css_class='mb-3'),
+            FloatingField('password2', css_class='mb-3')
+        )
+            
+        # Django form attributes / Field properties
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+        self.fields['password2'].label = 'Password Confirmation'
+
+class ThirdPartyForm(UserCreationForm):
     class Meta:
         model = ThirdParty
         fields = ['first_name', 'last_name', 'third_party_name', 'website', 'username', 'password1', 'password2']
+        labels = {
+            'first_name': 'First Name',
+            'last_name' : 'Last Name',
+            'email' : 'Email Address',
+        } # Password1 and Password2 must be relabled outside of lables
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Crispy Package Setup
+        self.helper = FormHelper()
+
+        # Bootstrap 
+        self.helper.layout = Layout(
+            FloatingField('first_name', css_class='mb-3'),
+            FloatingField('last_name', css_class='mb-3'),
+            FloatingField('third_party_name', css_class='mb-3'),
+            FloatingField('website', css_class='mb-3'),
+            FloatingField('email', css_class='mb-3'),
+            FloatingField('password1', css_class='mb-3'),
+            FloatingField('password2', css_class='mb-3')
+        )
+            
+        # Django form attributes / Field properties
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+        self.fields['password2'].label = 'Password Confirmation'
+        self.fields['third_party_name'].label = 'Third Party Name'
+
 
 class TimelineCreationForm(ModelForm):
     class Meta:
@@ -29,6 +111,5 @@ class EventCreationForm(ModelForm):
     class Meta:
         model = Event
         fields = ['name', 'description', 'location', 'deadline', 'file']
-        widgets = { 'timeline_id' : forms.HiddenInput }
-
-        
+        widgets = { 'timeline_id' : forms.HiddenInput 
+                    }
--- website/website/settings.py
+++ website/website/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'main', # MVC
     'dashboard', # dashboard app
 ]
