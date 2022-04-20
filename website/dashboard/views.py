71f041b133531d85b1a866cc8689e8d2db700f7d
Winston <winstonobonsu@gmail.com>
2022-04-20 02:14:45 -0400

    Testing passing context to dashboard/home

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -22,11 +22,11 @@ def createEvent(request, context):
 
 # Create your views here.
 def dashboard(response):
-    context = {'timeline_list' : [1, 2, 3, 4, 5, 6, 7, 8] }
+    context = {'timeline_list' : "[1, 2, 3, 4, 5, 6, 7, 8]" }
     return render(response, "dashboard/home.html", context)
 
 '''
-def timeline(request):
+def timeline(request):` `
     
     context = {}
     context.update({"event_form": EventForm()})
