b07e9b6880131c66f7dd4025b5bee934c0892324
Winston <winstonobonsu@gmail.com>
2022-04-21 01:30:54 -0400

    Assigned user

--- website/main/views.py
+++ website/main/views.py
@@ -54,8 +54,8 @@ def registerUser(response, accType):
     
     #if response.method == 'POST':
     if form.is_valid():
-        #user = form.save(commit=False) # Create the user object, but don't send it
-        form.save(commit=False) # benji test
+        user = form.save(commit=False) # Create the user object, but don't send it
+        # form.save(commit=False) # benji test
         #user.id = generateID(User)
         # username = response.POST.get('username') # benji test
         # password = response.POST.get('password1') # benji test
@@ -151,7 +151,6 @@ def createEvent(request, context):
 ### Define more functions for queries (Not sure if this is the right file for this)
 
 def index(response):
-    accType = 'Client'
 
     # Defult context for our page
     context = {}
