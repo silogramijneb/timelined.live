397b238e717bbc2df05e3c986ca37d47d9b1f8ba
Ivan Bondarenko <ivanbonddev@gmail.com>
2022-04-21 13:10:17 -0400

    update

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -29,10 +29,7 @@ def dashboard(response):
 
 def timeline(request):
 
-    context = {
-        "form": EventCreationForm(request.POST),
-        "maxEventNumbers": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-    }
+    context = {}
     
     # POST: Update context
     if request.method == "POST":
@@ -44,7 +41,10 @@ def timeline(request):
         if request.POST.get("action") == 'createEvent':
             createEvent(request, context)
             
-    result = render(request, "dashboard/timeline.html", context)            
+    result = render(request, "dashboard/timeline.html", {
+        'form': EventCreationForm(request.POST),
+        'maxEventNumbers': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+    })            
     return result
 
 
