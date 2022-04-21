5d902dafa08a4404fa07dbc33cc3bdad7f3abc58
Winston <winstonobonsu@gmail.com>
2022-04-21 14:21:14 -0400

    Replacing registration button with login form for test

--- website/main/views.py
+++ website/main/views.py
@@ -139,8 +139,8 @@ def index(response):
     
     #Load Registration Forms 
     context = {
-        'registration_form': ClientRegistrationForm(),
-        'login_form': LoginForm()
+        'registration_form': LoginForm(),
+        # 'login_form': 
         }
     
     # Render defult page with updated context
